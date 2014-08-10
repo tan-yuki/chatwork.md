64a694fbba44e2647df7619961699e73e3e3ce1a
tan-yuki <a0673040@gmail.com>
2014-08-10 18:18:53 +0900

    Add testcase

---
+++ app/scripts/util.js
@@ -0,0 +1,111 @@
+(function(root) {
+  'use strict';
+
+  var util = {};
+
+  /**
+   * Return true if this message has own markdown tag.
+   * 
+   * @param {String} Chat message.
+   */
+  util.hasMarkdownTag = function(str) {
+    return (/\[m\]([\s\S]*)\[\/m\]/g).test(str);
+  };
+
+  /**
+   * Revert <a> tag link to URL string.
+   * 
+   * @param {String} Chat message.
+   * @return {String} Chat message that has been converted.
+   */
+  util.revertHtmlLink= function(str) {
+    var newStr = str.replace(/\[m\]([\s\S]*)<a href="([\s\S]*)" title="[\s\S]*" target="_blank"[\s\S]*>[\s\S]*<\/a>([\s\S]*)*\[\/m\]/g, '[m]$1$2$3[/m]');
+    if (newStr !== str) {
+      return this.revertHtmlLink(newStr);
+    }
+
+    return str;
+  }
+
+  /**
+   * Revert emoticon tags to original string.
+   * 
+   * @param {String} Chat message.
+   * @return {String} Chat message that has been converted.
+   */
+  util.revertEmoticon = function(str) {
+    var newStr = str.replace(/\[m\]([\s\S]*)<[\s\S]*alt="(.*)"[\s\S]*class="ui_emoticon"[\s\S]*>([\s\S]*)*\[\/m\]/g, '[m]$1$2$3[/m]');
+
+    if (newStr !== str) {
+      return this.revertEmoticon(newStr);
+    }
+    return str;
+  };
+
+  /**
+   * Remove all html tags.
+   * 
+   * @param {String} Chat message.
+   * @return {String} Chat message that has been converted.
+   */
+  util.removeHtmlTag = (function() {
+    var $div = $('<div></div>');
+    return function(str) {
+      return $div.empty().html(str).text();
+    };
+  })();
+
+  /**
+   * Convert own markdown tag to html.
+   * 
+   * @param {String} Chat message.
+   * @return {String} Chat message that has been converted.
+   */
+  util.parseMarkdownTag = function(str) {
+    var $div = $('<div></div>');
+    return str.replace(/\[m\]([\s\S]*)\[\/m\]/g, function(str, p) {
+      var $pre = $('<pre></pre>').append(marked(p));
+      $pre.addClass('markdown');
+      return $div.empty().append($pre).html()
+    });
+  };
+
+  /**
+   * Convert message to markdown.
+   * 
+   * @param {Object} Chat message.
+   * @return {String} Chat message that has been converted.
+   */
+  util.convertMessage = function(str) {
+      str = util.revertHtmlLink(str);
+      str = util.revertEmoticon(str);
+      str = util.removeHtmlTag(str);
+      return util.parseMarkdownTag(str);
+  }
+
+  /**
+   * This function is called when chat timeline renderred.
+   * 
+   * @param {String} Timeline html.
+   * @return {String} Timeline html that has been converted.
+   */
+  util.onRenderTimeLine = function(timeline) {
+    return $('<div></div>').html(timeline).find('.chatTimeLineMessage').each(function(i, e) {
+      var $el = $(e).find('pre');
+      if (!$el.length) {
+        return;
+      }
+
+      var m = $el.html();
+
+      if (!hasMarkdownTag(m)) {
+        return;
+      }
+
+      m = util.convertMessage(m);
+      $el.html(m);
+    });
+  };
+
+  root.util = util;
+})(this);
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,113 @@
 /* global describe, it */
 
 (function () {
-    'use strict';
+  'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+  describe('util', function () {
+    describe('#hasMarkdownTag', function () {
+      it('should return true if string has markdown tag', function () {
+        expect(util.hasMarkdownTag('hogehoge\n[m]\nfoo\n[/m]')).to.be.true;
+      });
 
-            });
-        });
+      it('should return false if string has markdown tag', function () {
+        expect(util.hasMarkdownTag('hogehoge\n[info]\neee\n[/info]')).to.not.be.true;
+      });
     });
+
+    describe('#revertHtmlLink', function () {
+      it('should convert string contains html link in markdown tag', function () {
+        var message = [
+          '[m]',
+          'hogehoge',
+          '<a href="https://www.chatwork.com" title="https://www.chatwork.com" target="_blank" class="ui_sp_favicon_parent">https://www.chatwork.com</a>',
+          'fugafuga',
+          '[/m]',
+        ].join('\n');
+
+        var expected = [
+          '[m]',
+          'hogehoge',
+          'https://www.chatwork.com',
+          'fugafuga',
+          '[/m]',
+        ].join('\n');
+
+        expect(util.revertHtmlLink(message)).to.equal(expected);
+      });
+
+      it('should not convert string contains html link outside of the markdown tag', function () {
+        var message = [
+         'hogehoge',
+         '<a href="https://www.chatwork.com" title="https://www.chatwork.com" target="_blank" class="ui_sp_favicon_parent">https://www.chatwork.com</a>',
+         'fugafuga',
+        ].join('\n');
+
+        expect(util.revertHtmlLink(message)).to.equal(message);
+      });
+
+      it('should convert string contains several html links in markdown tag', function () {
+        var message = [
+          '[m]',
+          '<a href="https://www.google.co.jp" title="https://www.google.co.jp" target="_blank" class="ui_sp_favicon_parent">https://www.google.co.jp</a>',
+          'hogehoge',
+          '<a href="https://www.chatwork.com" title="https://www.chatwork.com" target="_blank" class="ui_sp_favicon_parent">https://www.chatwork.com</a>',
+          'fugafuga',
+          '[/m]',
+        ].join('\n');
+
+        var expected = [
+          '[m]',
+          'https://www.google.co.jp',
+          'hogehoge',
+          'https://www.chatwork.com',
+          'fugafuga',
+          '[/m]',
+        ].join('\n');
+
+        expect(util.revertHtmlLink(message)).to.equal(expected);
+      });
+    });
+
+    describe('#revertEmoticon', function () {
+      it('should convert string contains emoticon in markdown tag', function () {
+        var message = [
+          '[m]',
+          'hogehoge',
+          '<img src="./image/emoticon/emo_smile.gif" title="笑顔 :)" alt=":)" class="ui_emoticon">',
+          '<img src="./image/emoticon/emo_nod.gif" title="うんうん (nod)" alt="(nod)" class="ui_emoticon">',
+          'fugafuga',
+          '[/m]',
+        ].join('\n');
+
+        var expected = [
+          '[m]',
+          'hogehoge',
+          ':)',
+          '(nod)',
+          'fugafuga',
+          '[/m]',
+        ].join('\n');
+
+        expect(util.revertEmoticon(message)).to.equal(expected);
+      });
+
+      it('should not convert string contains emoticon out of markdown tag', function () {
+        var message = [
+          'hogehoge',
+          '<img src="./image/emoticon/emo_smile.gif" title="笑顔 :)" alt=":)" class="ui_emoticon">',
+          '<img src="./image/emoticon/emo_nod.gif" title="うんうん (nod)" alt="(nod)" class="ui_emoticon">',
+          'fugafuga',
+        ].join('\n');
+
+        expect(util.revertEmoticon(message)).to.equal(message);
+      });
+    });
+
+    describe('#removeHtmlTag', function () {
+      it('should remove html tag in string', function () {
+        var message = 'hogehoge<strong>foo!</strong>';
+        expect(util.removeHtmlTag(message)).to.equal('hogehogefoo!');
+      });
+    });
+  });
 })();
